de0e176ca9eb16a9f03f5af3b4854be16a81ee1f
Kaz Schmanski <kschmanski1@gmail.com>
2020-10-22 22:11:04 -0500

    add bootstrap and new movie-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import {FormsModule} from "@angular/forms";
+
+const routes: Routes = [
+  {path: 'movie-detail', component:MovieDetailComponent},
+  {path: 'todo', component:TodoListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
