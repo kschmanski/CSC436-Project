8eb35ea7d6a9247dd6128edd33619e42b1f1e25f
Edward Lin <edward30525@gmail.com>
2020-10-24 22:35:04 -0500

    Added component-movie-search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import {FormsModule} from "@angular/forms";
+import { MovieSearchComponent } from './movie-search/movie-search.component';
 
 const routes: Routes = [
   {path: 'movie-detail', component:MovieDetailComponent},
-  {path: 'todo', component:TodoListComponent}
+  {path: 'todo', component:TodoListComponent},
+  {path: 'search-movie', component: MovieSearchComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieDetailComponent,
-    TodoListComponent
+    TodoListComponent,
+    MovieSearchComponent
   ],
   imports: [
     BrowserModule,
