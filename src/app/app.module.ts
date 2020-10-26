51edd2d04cb0cd73d4afaa68143182ca159d278b
afiae <afiaekra@gmail.com>
2020-10-26 13:27:37 -0500

    home page path fixed, added favorites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppComponent } from './app.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { ForumPageComponent } from './forum-page/forum-page.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'movie-detail', component:MovieDetailComponent},
-  {path: 'todo', component:TodoListComponent},
+  {path: 'movie-detail', component: MovieDetailComponent},
+  {path: 'todo', component: TodoListComponent},
   {path: 'search-movie', component: MovieSearchComponent},
-  {path: 'forum', component:ForumPageComponent}
+  {path: 'forum', component: ForumPageComponent},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 @NgModule({
@@ -22,7 +27,9 @@ const routes: Routes = [
     MovieDetailComponent,
     TodoListComponent,
     MovieSearchComponent,
-    ForumPageComponent
+    ForumPageComponent,
+    FavoritesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
