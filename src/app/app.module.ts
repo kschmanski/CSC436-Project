30e5ad2866d6330f6079a4035d8128223163a277
Michael Guzman <mgzmn4@gmail.com>
2020-10-25 19:18:56 -0500

    Created forum component. Added to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import {FormsModule} from "@angular/forms";
+import { ForumPageComponent } from './forum-page/forum-page.component';
 
 const routes: Routes = [
   {path: 'movie-detail', component:MovieDetailComponent},
-  {path: 'todo', component:TodoListComponent}
+  {path: 'todo', component:TodoListComponent},
+  {path: 'forum', component:ForumPageComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieDetailComponent,
-    TodoListComponent
+    TodoListComponent,
+    ForumPageComponent
   ],
   imports: [
     BrowserModule,
