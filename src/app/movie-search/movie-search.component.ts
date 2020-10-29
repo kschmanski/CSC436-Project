41becaddbeb17cca3c9c54515daac01d61e28a17
Kaz Schmanski <kschmanski1@gmail.com>
2020-10-28 20:10:37 -0500

    comment out some code from old api

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -27,25 +27,28 @@ export class MovieSearchComponent implements OnInit {
   ngOnInit() {
   }
   onSubmit(form): void {
-    console.log(form.value);
-    this.http.get<any>(settings.url, settings).subscribe(data => {
-      console.log(data);
-    });
+   // console.log(form.value);
+   // this.http.get<any>(settings.url, settings).subscribe(data => {
+   //   console.log(data);
+   // });
 
+    // @ts-ignore
     this.http.get<any>(settings2.url, settings2).subscribe(data => {
       console.log(data);
     })
+
   }
 
 
 
 }
 
+
 var settings2 = {
   "async": true,
   "crossDomain": true,
   //"url": "http://www.omdbapi.com/?apikey=[yourkey]&t=good&plot=full",
-  "url": "  http://www.omdbapi.com/?i=tt3896198&apikey=430ac435&t=good&plot=full",
+  "url": "http://www.omdbapi.com/?i=tt3896198&apikey=430ac435&t=good&plot=full",
   "method": "GET",
 }
 
@@ -53,7 +56,7 @@ var settings = {
   "async": true,
   "crossDomain": true,
   //"url": "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Avengers%20Endgame",
-  "url": "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Good%20Will%20Hunting",
+  "url": "https://movie-database-imdb-alternative.p.rapidapi.com/?r=json&s=Good%20Will%20Hunting",
   "method": "GET",
   "headers": {
     "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
