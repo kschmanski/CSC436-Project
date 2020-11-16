08e6dbfbfbac7be09716925ef60b5f2c294966b2
Kaz Schmanski <kschmanski1@gmail.com>
2020-11-15 19:17:37 -0600

    clean up moviesearchcomponent class

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import {Component, OnInit, ViewChild} from '@angular/core';
-import { map } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
 import { NgModule } from '@angular/core';
-import {NgForm, FormGroup, FormControl, Validators} from '@angular/forms';
+import {NgForm, FormControl} from '@angular/forms';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,7 +11,6 @@ import { InteractionService } from '../interaction.service';
 export class SearchQuery {
   //the string the user passed in from the search bar
   public searchQueryFromUser: string = '';
-  public message: string = '';
 
 }
 @NgModule({
@@ -43,94 +41,35 @@ export class MovieSearchComponent implements OnInit {
     return settings.url_without_movie_title + movie_title_to_search + "&plot=full";
   }
 
-  constructor(private http: HttpClient, private _interactionService: InteractionService
-
-  ) {
-  }
+  constructor(private http: HttpClient, private _interactionService: InteractionService) { }
 
   name = new FormControl('');
-  name2 = new NgForm(null, null);
-
   result: any;
-  msg: any;
 
   ngOnInit() {
-    // this._interactionService.message$.subscribe(
-    //   message => {
-    //       this.msg = this._interactionService.getMessage();
-    //       console.log('Movie Search Component received Message: ' + this._interactionService.getMessage());
-    //       console.log('this.msg is now ' + this.msg);
-    //       this.myFunction(this.msg);
-    //   }
-    // )
   }
 
-  myFunction(message : string) {
-    console.log('myFunction: ' + message);
-
-    this.name.setValue(message);
-    //this.name2.controls['message'].setValue(message);
-    //this.name2.setValue(message);
-    this.name2.controls.state.setValue(message);
-
-    console.log('calling API');
-    var apiStringToQuery = this.getApiStringForMovie(api_settings, message);
-    console.log(apiStringToQuery);
-
-    // @ts-ignore
-    this.http.get<any>(apiStringToQuery, api_settings).subscribe(api_data => {
-      console.log(api_data);
-      console.log(api_data[0]);
-      this.result = api_data.Search;
-    })
-  }
 
   model = new SearchQuery();
 
   onSubmit(f: NgForm): void {
-
     var apiStringToQuery = this.getApiStringForMovie(api_settings, f.value.searchQueryFromUser);
-    //var apiStringToQuery = this.getApiStringForMovie(api_settings, this.name.value);
 
     // @ts-ignore
     this.http.get<any>(apiStringToQuery, api_settings).subscribe(api_data => {
-        //console.log(api_data);
-        //console.log(api_data[0]);
         this.result = api_data.Search;
       })
 
   }
 
+  /**
+   * Stores the incoming movie title from the search page and sends it to this._interactionService
+   *
+   * @param movieTitle title of the movie coming from the search page.
+   */
   redirectFromSearchToDetail(movieTitle) {
-
-    //console.log('redirect from search to detail');
-
-    console.log('title is: ');
-    console.log(movieTitle);
-
-    console.log('calling setCookie');
-    //this.setCookie(movieObject);
     localStorage.setItem('movie-title', movieTitle);
-
-    console.log(localStorage.getItem('movie-title'));
-
-    //document.cookie="movie-name='" + title + "';";
-
-    //console.log(document.cookie);
     this._interactionService.sendMessage(movieTitle);
-
-  }
-
-  setCookie(title) {
-    console.log('setting cookie');
-    var d = new Date();
-
-    d.setTime(d.getTime() + 100);
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = "movie-name" + "=" + title + ";" + expires + ";path=/";
-
-    console.log('all finished setting cookie');
-    console.log('cookie is ' + document.cookie);
   }
 }
 
