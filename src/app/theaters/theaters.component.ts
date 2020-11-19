b35d185d73fb8e502bedf2bd57127a1bde09493f
Kaz Schmanski <kschmanski1@gmail.com>
2020-11-18 19:53:52 -0600

    update text

--- src/app/theaters/theaters.component.ts
+++ src/app/theaters/theaters.component.ts
@@ -3,6 +3,7 @@ import {GoogleMapsModule} from "@angular/google-maps";
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import {AppComponent} from "../app.component";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],  imports: [BrowserModule, GoogleMapsModule],
@@ -16,28 +17,11 @@ import {AppComponent} from "../app.component";
   styleUrls: ['./theaters.component.css']
 })
 export class TheatersComponent implements OnInit {
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 41.8781; //Chicago Latitude
+  lng = -87.6298; //Chicago Longitude
   constructor() { }
 
-  result: any;
-  center: google.maps.LatLngLiteral
-
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-    })
   }
 
-}
-
-var google_api_settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://www.google.com/maps/@?api=1&map_action=map",
-  "method": "GET",
-  "key": "AIzaSyAC9PRQfTqQD6F5BdlbzFRF2wnuf_WdkTM"
 }
